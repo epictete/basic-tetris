3e9a3c7a7e83c9bde4b9e9ae4589346be24afffa
epictete <63167666+epictete@users.noreply.github.com>
2020-05-21 22:13:48 +0200

    Made some basic styling

--- app.js
+++ app.js
@@ -212,9 +212,9 @@ document.addEventListener('DOMContentLoaded', () => {
   //game over
   function gameOver() {
     if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      scoreDisplay.innerHTML = 'end'
+      scoreDisplay.innerHTML = 'Game Over'
       clearInterval(timerId)
     }
   }
-  
+
 })
